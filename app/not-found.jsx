41b7490928fee6dd543ef26786bfa95410ca437d
Feqan00 <ehmedovfeqan18@gmail.com>
2024-01-19 15:13:01 +0400

    first-commit

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const notFound = () => {
+    return (
+        <div style={{ textAlign: 'center' }} className='notFound'>NotFound</div>
+    )
+}
+
+export default notFound
---
+++ components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Box, Stack } from '@mui/material'
+import NavLogo from '../../app/assets/logo/LogoDelizioso.png'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <Stack>
+
+        </Stack>
+    )
+}
+
+export default Header
